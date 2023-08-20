570dcae45cfd439266662e0602ca6f86b7d9dae1
Daniel Lake <90655952+laked0601@users.noreply.github.com>
2023-08-20 16:02:32 +0100

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set project_name=
+set user_name=
+
+rem Remove the existing directory
+rmdir /s /q %project_name%
+
+rem Clone the repository
+git clone https://github.com/%user_name%/%project_name%
+
+rem Create a virtual environment
+python -m venv ".\%project_name%\env"
+
+rem Install the required packages
+".\%project_name%\env\Scripts\python.exe" -m pip install -r ".\%project_name%\requirements.txt"
+
+rem Add project directory to path on running of each file
+
+set customize_path=.\%project_name%\env\Lib\site-packages\sitecustomize.py
+
+echo from sys import path > %customize_path%
+echo from os.path import abspath, dirname, join >> %customize_path%
+echo script_directory = abspath(dirname(__file__)) >> %customize_path%
+echo target_directory = abspath(join(script_directory, pardir, pardir, pardir)) >> %customize_path%
+echo if target_directory not in path: >> %customize_path%
+echo     path.extend([target_directory]) >> %customize_path%
